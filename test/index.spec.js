848899aa652e7d5bd9e7bb473d4e737ca7d7f981
Christian Lohr <christian.lohr@gmail.com>
2014-08-29 21:08:50 -0400

    Fixed zombie test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,11 +1,34 @@
 'use strict';
 
 var Browser = require('zombie');
+var Hapi = require('hapi');
+var config = require('../config');
+var routes = require('../routes')();
+
+var server;
+
+before(function(done) {
+    // create server instance
+    server = Hapi.createServer('localhost', config.port, config.options);
+    // routes
+    server.route(routes);
+    // start server
+    server.start(function () {
+        console.log('Hapi server started');
+        done();
+    });
+});
+
+after(function(done){
+    server.stop();
+    console.log('Hapi server stopped');
+    done();
+});
 
 setup(function(done) {
     this.browser = Browser.create();
     this.browser.visit('http://localhost:3000').then(done, done);
-    this.browser.on("error", function(error) {
+    this.browser.on('error', function(error) {
         console.error(error);
     });
 });
